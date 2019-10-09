4c0312ac04e3c474f1c6e2f50d754f2c69e37dc8
Brennan Neilson <bvneilson@gmail.com>
2019-10-08 21:27:04 -0600

    Starting on form.js

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Form from './components/Form.js';
 import TeamMemberList from './components/TeamMemberList.js';
 
 function App() {
   return (
     <div className="App">
+      <Form />
       <TeamMemberList />
     </div>
   );
---
+++ team-builder/src/components/Form.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Form = (props) => {
+
+  return (
+    <form>
+
+    </form>
+  );
+}
+
+export default Form;
